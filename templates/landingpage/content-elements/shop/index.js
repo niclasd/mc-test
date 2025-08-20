591c85de9f45cada2547930c0ebb564193d715cc
niclasd <niclas@deissler-partner.de>
2025-08-20 16:52:55 +0200

    Add shop parts

--- templates/landingpage/content-elements/shop/index.js
+++ templates/landingpage/content-elements/shop/index.js
@@ -3,4 +3,12 @@ const {cx} = require('@bsi-cx/design-build');
 module.exports = cx.contentElement
   .withElementId('shop-UEyFnQ')
   .withLabel('Shop')
+  .withParts(
+    cx.part.withPartId('product-list').withLabel('Produktliste'),
+    cx.part.withPartId('cart-heading').withLabel('Warenkorb-Überschrift'),
+    cx.part.withPartId('cart-empty-button').withLabel('Warenkorb leeren'),
+    cx.part.withPartId('point-warning').withLabel('Punktwarnung'),
+    cx.part.withPartId('lightbox-close-text').withLabel('Lightbox schließen'),
+    cx.part.withPartId('modal-ok-button').withLabel('OK-Schaltfläche')
+  )
   .withFile(require('./template.twig'));
