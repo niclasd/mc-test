b633270f704f76d4a8747f58b76367520c960ef3
niclasd <niclas@deissler-partner.de>
2025-08-20 17:23:18 +0200

    refactor: use cx.part for shop content element

--- templates/landingpage/content-elements/shop/index.js
+++ templates/landingpage/content-elements/shop/index.js
@@ -1,14 +1,14 @@
-const {cx, part} = require('@bsi-cx/design-build');
+const { cx } = require('@bsi-cx/design-build');
 
 module.exports = cx.contentElement
   .withElementId('shop-UEyFnQ')
   .withLabel('Shop')
   .withParts(
-    part.withId('product-list').withLabel('Produktliste'),
-    part.withId('cart-heading').withLabel('Warenkorb-Überschrift'),
-    part.withId('cart-empty-button').withLabel('Warenkorb leeren'),
-    part.withId('point-warning').withLabel('Punktwarnung'),
-    part.withId('lightbox-close-text').withLabel('Lightbox schließen'),
-    part.withId('modal-ok-button').withLabel('OK-Schaltfläche')
+    cx.part.withId('product-list').withLabel('Produktliste'),
+    cx.part.withId('cart-heading').withLabel('Warenkorb-Überschrift'),
+    cx.part.withId('cart-empty-button').withLabel('Warenkorb leeren'),
+    cx.part.withId('point-warning').withLabel('Punktwarnung'),
+    cx.part.withId('lightbox-close-text').withLabel('Lightbox schließen'),
+    cx.part.withId('modal-ok-button').withLabel('OK-Schaltfläche')
   )
   .withFile(require('./template.twig'));
