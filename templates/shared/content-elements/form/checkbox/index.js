db14b8a6d657c6867306b452d252b168311e850e
niclasd <niclas@deissler-partner.de>
2025-08-21 09:30:44 +0200

    Delete templates/shared/content-elements/form directory

--- templates/shared/content-elements/form/checkbox/index.js
+++
@@ -1,3 +0,0 @@
-require('./styles.scss');
-
-module.exports = require('./prototype')();
--- templates/shared/content-elements/form/radio/prototype/radio.js
+++
@@ -1,8 +0,0 @@
-import Alpine from '@alpinejs/csp';
-
-Alpine.data('formRadio', () => ({
-  validateInput() {
-    let valid = this.$el.checkValidity();
-    this.$root.querySelector('.invalid-feedback').style.display = valid ? 'none' : 'block';
-  }
-}))
